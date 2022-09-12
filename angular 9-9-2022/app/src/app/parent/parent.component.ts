4a504828c2f0f0c0f545fbd3802da24ed8530626
Nistha Patel <nistha.patel@1Rintern.local>
2022-09-11 22:32:56 -0700

    update

---
+++ angular 9-9-2022/app/src/app/parent/parent.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.scss']
+})
+export class ParentComponent implements OnInit {
+
+  public default_size = 100
+  public fontsize: number;
+
+  constructor() {
+    this.fontsize = this.default_size;
+  }
+
+  ngOnInit(): void {
+  }
+
+  getvalue(type: string) {
+
+    this.fontsize = (type === 'increase') ? this.fontsize + 10 : this.fontsize - 10;
+
+    if (this.fontsize == 200) {
+      this.fontsize = this.default_size;
+    }
+    if (this.fontsize == 0) {
+      this.default_size = this.default_size;
+    }
+  }
+}
